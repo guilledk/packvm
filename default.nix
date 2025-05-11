39b7725f77593e84a5dab5bfcee6cb14c57698a1
Guillermo Rodriguez <guillermo@telos.net>
2025-05-11 15:48:48 -0300

    Add nixos dev env file

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  nativeBuildInputs = with pkgs; [
+    openssl pkg-config
+    stdenv.cc.cc.lib
+    uv
+  ];
+
+in
+pkgs.mkShell {
+  inherit nativeBuildInputs;
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
+  TMPDIR = "/tmp";
+}
